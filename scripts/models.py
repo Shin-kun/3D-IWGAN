993bc2b7aa0f861315089a3cd2da399e23bda37b
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2019-05-24 21:19:27 +0800

    improve model

--- scripts/models.py
+++ scripts/models.py
@@ -8,16 +8,18 @@ from tensorlayer.layers import *
 import random 
 from glob import glob
 
-
-def Deconv(inputs, f_dim_in, dim, net, batch_size, f_dim_out = None, stride = 2 ):
+def Deconv(inputs, f_dim_in, dim, net, batch_size, f_dim_out = None, stride = 2, batch_norm = False):
 	if f_dim_out is None: 
 		f_dim_out = f_dim_in/2 
-	return tl.layers.DeConv3dLayer(inputs,
-								shape = [4, 4, 4, f_dim_out, f_dim_in],
-								output_shape = [batch_size, dim, dim, dim, f_dim_out],
-								strides=[1, stride, stride, stride, 1],
-								W_init = tf.random_normal_initializer(stddev=0.02),
-								act=tf.identity, name='g/net_' + net + '/deconv')
+	layer =  tl.layers.DeConv3dLayer(inputs,
+				shape = [4, 4, 4, f_dim_out, f_dim_in],
+				output_shape = [batch_size, dim, dim, dim, f_dim_out],
+				strides=[1, stride, stride, stride, 1],
+				W_init = tf.random_normal_initializer(stddev=0.02),
+				act=tf.identity, name='g/net_' + net + '/deconv')
+	if batch_norm:
+		return tl.layers.BatchNormLayer(layer, is_train=is_train, name='g/net_' + net + '/batch_norm')
+	return layer
 
 def Conv3D(inputs, f_dim_out, net, f_dim_in = None, batch_norm = False, is_train = True):
 	if f_dim_in is None: 
@@ -105,31 +107,19 @@ def generator_DCGAN(inputs, is_train=True, reuse=False, batch_size = 128):
 		net_1 = tl.layers.DenseLayer(net_0, 
 			n_units= gf_dim*forth*forth*forth,
 			W_init = tf.random_normal_initializer(stddev=0.02),
-			act=tf.identity,
 			name='g/net_1/dense')
 		net_1 = tl.layers.ReshapeLayer(net_1, shape = [-1, forth, forth, forth, gf_dim], name='g/net_1/reshape')
 		net_1 = tl.layers.BatchNormLayer(net_1, 
 			is_train=is_train, 
 			gamma_init=tf.random_normal_initializer(1., 0.02), 
 			name='g/net_1/batch_norm')
-		# net_1 = tl.layers.DropoutLayer(net_1, keep=keep_prob, name='g/net_1/drop', is_train = is_train)
-		net_1.outputs = tf.nn.relu(net_1.outputs, name='g/net_1/relu')
+		net_1.outputs = tf.nn.leaky_relu(net_1.outputs, name='g/net_1/lrelu')
 
-		net_2 = Deconv(net_1, gf_dim, half, '2', batch_size)
-		net_2 = tl.layers.BatchNormLayer(net_2,
-			is_train=is_train,
-			gamma_init=tf.random_normal_initializer(1., 0.02), 
-			name='g/net_2/batch_norm')
-		# net_2 = tl.layers.DropoutLayer(net_2, keep=keep_prob, name='g/net_2/drop', is_train = is_train)
-		net_2.outputs = tf.nn.relu(net_2.outputs, name='g/net_2/relu')
+		net_2 = Deconv(net_1, gf_dim, half, '2', batch_size, batch_norm=True)
+		net_2.outputs = tf.nn.leaky_relu(net_2.outputs, name='g/net_2/lrelu')
 
-		net_3 = Deconv(net_2, gf_dim/2, output_size, '3', batch_size)
-		net_3 = tl.layers.BatchNormLayer(net_3,
-			is_train=is_train,
-			gamma_init=tf.random_normal_initializer(1., 0.02), 
-			name='g/net_3/batch_norm')
-		# net_3 = tl.layers.DropoutLayer(net_3, keep=keep_prob, name='g/net_3/drop', is_train = is_train)
-		net_3.outputs = tf.nn.relu(net_3.outputs, name='g/net_3/relu')
+		net_3 = Deconv(net_2, gf_dim/2, output_size, '3', batch_size, batch_norm=True)
+		net_3.outputs = tf.nn.leaky_relu(net_3.outputs, name='g/net_3/lrelu')
 
 		net_4 = Deconv(net_3, gf_dim/4, output_size, '4', batch_size, f_dim_out = 1, stride = 1) 
 		net_4.outputs = tf.reshape(net_4.outputs,[batch_size,output_size,output_size,output_size], name='g/net_4/reshape')
@@ -148,51 +138,18 @@ def discriminator_DCGAN(inputs ,output_size, improved = False, VAE_loss = False,
 		net_0 = tl.layers.InputLayer(inputs, name='d/net_0/in')
 
 		net_1 = Conv3D(net_0, df_dim, '1', f_dim_in=1, batch_norm = False)
-		# net_1 = tl.layers.DropoutLayer(net_1, keep=keep_prob, name='d/net_1/drop', is_train = is_train)
 		net_1.outputs = tf.nn.leaky_relu(net_1.outputs, alpha=0.2, name='d/net_1/lrelu')
-
-		b1 = tl.layers.BatchNormLayer(net_1,
-			is_train=is_train,
-			gamma_init=tf.random_normal_initializer(1., 0.02), 
-			name='d/net_1/batch_norm'
-		)
-		b1.outputs = tf.nn.leaky_relu(b1.outputs, alpha=0.2, name='d/net_1/batch_norm/lrelu')
-	
-		
+			
 		net_2 = Conv3D(net_1, df_dim*2, '2', batch_norm = not improved, is_train = is_train)
 		net_2.outputs = tf.nn.leaky_relu(net_2.outputs, alpha=0.2, name='d/net_2/lrelu')
-
-		b1 = tl.layers.BatchNormLayer(net_2,
-			is_train=is_train,
-			gamma_init=tf.random_normal_initializer(1., 0.02), 
-			name='d/net_2/batch_norm'
-		)
-		b1.outputs = tf.nn.leaky_relu(b1.outputs, alpha=0.2, name='d/net_2/batch_norm/lrelu')
-		# b1 = tl.layers.DropoutLayer(b1, keep=keep_prob, name='d/net_2/drop', is_train= is_train)
 		
-		net_3 = Conv3D(b1, df_dim*4, '3', batch_norm = not improved, is_train = is_train)
+		net_3 = Conv3D(net_2, df_dim*4, '3', batch_norm = not improved, is_train = is_train)
 		net_3.outputs = tf.nn.leaky_relu(net_3.outputs, alpha=0.2, name='d/net_3/lrelu')
 
-		b2 = tl.layers.BatchNormLayer(net_3,
-			is_train=is_train,
-			gamma_init=tf.random_normal_initializer(1., 0.02), 
-			name='d/net_3/batch_norm'
-		)
-		b2.outputs = tf.nn.leaky_relu(b2.outputs, alpha=0.2, name='d/net_3/batch_norm/lrelu')
-		# b2 = tl.layers.DropoutLayer(b2, keep=keep_prob, name='d/net_3/drop', is_train = is_train)
-		
-		net_4 = Conv3D(b2, df_dim*8, '4', batch_norm = not improved, is_train = is_train)
+		net_4 = Conv3D(net_3, df_dim*8, '4', batch_norm = not improved, is_train = is_train)
 		net_4.outputs = tf.nn.leaky_relu(net_4.outputs, alpha=0.2, name='d/net_4/lrelu')
-
-		b3 = tl.layers.BatchNormLayer(net_4,
-			is_train=is_train,
-			gamma_init=tf.random_normal_initializer(1., 0.02), 
-			name='d/net_4/batch_norm'
-		)
-		b3.outputs = tf.nn.leaky_relu(b3.outputs, alpha=0.2, name='d/net_4/batch_norm/lrelu')
-		# b3 = tl.layers.DropoutLayer(b3, keep=keep_prob, name='d/net_4/drop', is_train= is_train)
 		
-		net_5 = FlattenLayer(b3, name='d/net_5/flatten')
+		net_5 = FlattenLayer(net_4, name='d/net_5/flatten')
 		net_5 = tl.layers.DenseLayer(net_5, 
 			n_units=output_units,
 			W_init = tf.random_normal_initializer(stddev=0.02),
